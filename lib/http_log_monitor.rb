ea23371b7b5d14597de692b3ad13e44bec28beef
Rafael George <george.rafael@gmail.com>
2018-10-29 19:23:44 -0400

    Initial setup

---
+++ lib/http_log_monitor.rb
@@ -0,0 +1,5 @@
+require "http_log_monitor/version"
+
+module HttpLogMonitor
+  # Your code goes here...
+end
---
+++ spec/http_log_monitor_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe HttpLogMonitor do
+  it "has a version number" do
+    expect(HttpLogMonitor::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
