1c67bb46eff7284f692912db12b1431839ab9131
Rafael George <george.rafael@gmail.com>
2018-11-16 08:29:02 -0500

    Extract method to separate concerns

--- lib/http_log_monitor.rb
+++ lib/http_log_monitor.rb
@@ -9,25 +9,34 @@ module HttpLogMonitor
     monitor = Monitor.new(options)
     report = Report.new(monitor: monitor)
 
-    f = File.open(monitor.file_path, "r")
+    file = build_file_for(monitor.file_path)
+    process_report_with(report)
+    process_file_content(file, monitor, report)
+  rescue Interrupt
+    report.to_s
+  end
 
+  def self.build_file_for(file_path)
+    File.open(file_path, "r")
+  rescue Errno::ENOENT
+    puts "#{file_path} doesn't exist"
+  end
+
+  def self.process_report_with(report)
     Thread.new do
       report.render
     end
+  end
 
+  def self.process_file_content(file, monitor, report)
     loop do
-      select([f])
-      line = f.gets
-
+      select([file])
+      line = file.gets
       Thread.new do
         log = Log.for(line.to_s)
         monitor = monitor.process(log)
         report.with(monitor)
-      end.join
+      end
     end
-  rescue Errno::ENOENT
-    puts "#{monitor.file_path} doesn't exist"
-  rescue Interrupt
-    report.to_s
   end
 end
