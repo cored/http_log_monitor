64e6f088e56f1a75927bfe5d7df15dae1306b112
Rafael George <george.rafael@gmail.com>
2018-11-19 09:40:02 -0500

    Add boolean flag to handle high traffic alerts
    This was needed to make the alert message more descriptive

--- lib/http_log_monitor/alert.rb
+++ lib/http_log_monitor/alert.rb
@@ -2,19 +2,29 @@ module HttpLogMonitor
   class Alert < Dry::Struct
     def with(hits:, threshold:)
       if hits >= threshold
-        new(hits: hits, status: "High Traffic", threshold: threshold)
+        new(hits: hits, threshold: threshold, high: true)
       else
-        new(hits: hits, status: "No Alerts - Recover", threshold: threshold)
+        new(hits: hits, threshold: threshold)
       end
     end
 
     attribute :hits, Types::Integer.default(0)
     attribute :time, Types::Time.default(Time.now)
-    attribute :status, Types::String.default("No Alerts - Recover")
+    attribute :high, Types::Bool.default(false)
     attribute :threshold, Types::Integer.default(10)
 
     def to_s
-      "#{status} at (#{time.strftime("%H:%M:%S %p")}) with hits: #{hits} - threshold: #{threshold}"
+      if high
+        "High Traffic at (#{formatted_time}) with hits: #{hits} - threshold: #{threshold}"
+      else
+        "No Alerts - Recover at (#{formatted_time})"
+      end
+    end
+
+    private
+
+    def formatted_time
+      time.strftime("%H:%M:%S %p")
     end
   end
 end
